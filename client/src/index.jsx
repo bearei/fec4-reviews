25503577679380d85b40b17e70a423ee773e7adf
rdkerr <rogerdkerr@gmail.com>
2019-02-07 15:56:04 -0700

    Rough skeleton

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -9,7 +9,47 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>HREI Reviews</div>
+      <div>
+        <div id='nav'>
+        <h1>HREI Reviews</h1><span>WRITE</span>
+          <div id='snapshot'>
+            <h1>Rating Snapshot</h1>
+            <h2>Select a row below to filter</h2>
+            <h3>5 * * * - -</h3>
+            <h3>4 * - - - -</h3>
+            <h3>3 - - - - -</h3>
+            <h3>2 - - - - -</h3>
+            <h3>1 * - - - -</h3>
+            <h2>1 - 2 of 2 Reviews</h2>
+          </div>
+          <div id='averages'>
+            <h1>Average Customer Rating</h1>
+            <h1>* * * - -</h1>
+            <h1>Fit   |  |  |  ||| |</h1>
+            <p>Runs Small    Runs Large</p>
+            <h1>Sort By Most Relvant</h1>
+          </div>
+        </div>
+          <h1>------------------------------</h1>
+        <div id='reviews'>
+          <div id='review'>
+            <h1>* * * * - Bill - 2 Days ago</h1>
+            <h2>Some title</h2>
+            <h3>SOme text Some more text</h3>
+            <h3> v x I recommend</h3>
+            <h3>Helfpul Yes No Report as inappropriate</h3> 
+          </div>
+          <h1>------------------------------</h1>
+          <div id='review'>
+            <h1>* * * * - Bill - 2 Days ago</h1>
+            <h2>Some title</h2>
+            <h3>SOme text Some more text</h3>
+            <h3> v x I recommend</h3>
+            <h3>Helfpul Yes No Report as inappropriate</h3> 
+          </div>
+        </div>
+        <h1>------------------------------</h1>
+      </div>
     );
   }
 }
