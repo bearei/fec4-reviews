f2742f19c0d5f71cd3e0ba635495a9454ce9a2be
rdkerr <rogerdkerr@gmail.com>
2019-02-11 14:59:15 -0700

    Refactored Modal to have state

--- client/components/Modal.jsx
+++ client/components/Modal.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
+class Modal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
 
-const Modal = (props) => {
-  const visibility = props.show ? 'modal display-block' : 'modal display-none';
-  return (
-    <div className={visibility}>
-      <div className='modal-main'>
-        <h1>MODAL</h1>
-        <p>Form Stuff</p>
-        <p>More form stuff</p>
-        <button onClick={props.handleClose}> close</button>
+  render() {
+    let visibility = this.props.show ? 'modal display-block' : 'modal: display: none';
+    return (
+      <div className={visibility}>
+        <div className='modal-main'>
+          <h1>MODAL</h1>
+          <p>Form Stuff</p>
+          <p>More form stuff</p>
+          <button onClick={this.props.handleClose}> close</button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Modal;
