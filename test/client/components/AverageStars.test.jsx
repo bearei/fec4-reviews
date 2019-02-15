8e231db03ad848b824ab25cad195671dc228065d
rdkerr <rogerdkerr@gmail.com>
2019-02-14 17:19:48 -0700

    Installed babel-jest, babel-plugin-rewire, added tests

---
+++ test/client/components/AverageStars.test.jsx
@@ -0,0 +1,17 @@
+const getIcon = require('../../../client/components/AveragesStars.jsx').__get__('getIcon');
+
+describe('GetIcon Test', () => {
+  describe('getIcon', () => {
+    it('should return star', () => {
+      const expected = 'star';
+      const actual = getIcon(1, 3);
+      expect(actual).toEqual(expected);
+    });
+
+    it('should return half star', () => {
+      const expected = 'star-half';
+      const actual = getIcon(3, 2.5);
+      expect(actual).toEqual(expected);
+    });
+  });
+});
