419689f8071af99a6acbe67d5d9cd98cf6e98b6d
rdkerr <rogerdkerr@gmail.com>
2019-02-14 20:08:52 -0700

    Refactored Stars out of containing module

--- client/components/AveragesStars.jsx
+++ client/components/AveragesStars.jsx
@@ -1,32 +1,12 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-const getIcon = (number, average) => {
-  let result;
-  if (number <= Math.floor(average)) {
-    result = 'star';
-  } else if (number <= Math.ceil(average)) {
-    result = 'star-half';
-  } else {
-    result = 'star';
-  }
-  return result;
-};
+import Stars from './Stars';
 
 const AveragesStars = (props) => {
   const classes = { ...props };
-  const numbers = Array.from({ length: 5 }, (v, k) => k + 1);
   return (
     <span>
       <span>Overall</span>
-      {numbers.map(number => (
-        <FontAwesomeIcon
-          icon={getIcon(number, classes.average)}
-          size="lg"
-          className={number > Math.ceil(classes.average) ? 'background-star' : 'foreground-star'}
-        />
-      ))
-      }
+      <Stars average={classes.average} />
       <span>{classes.average}</span>
     </span>
   );
--- client/components/ReviewListItem.jsx
+++ client/components/ReviewListItem.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Stars from './Stars';
 
 const ReviewListItem = (props) => {
   const classes = { ...props };
   return (
     <div>
       <div id="review">
-        <h1>Rating: {classes.review.rating} {classes.review.name} - {classes.review.createdAt}</h1>
+        <Stars average={classes.review.rating} />
+        <h1>{classes.review.name} - {classes.review.createdAt}</h1>
         <h2>{classes.review.title}</h2>
         <h3>{classes.review.text}</h3>
         <h3> {classes.review.recommend} I recommend</h3>
