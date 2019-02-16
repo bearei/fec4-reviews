bcc5f977ba40d30ff3f33661d1dc5f8a1a953721
rdkerr <rogerdkerr@gmail.com>
2019-02-16 13:52:55 -0700

    Formatting

--- client/components/util/RatingBar.jsx
+++ client/components/util/RatingBar.jsx
@@ -5,7 +5,6 @@ const RatingBar = ({ size }) => (
   <span id="rating-bar">
     <div className="rating-bar">
       <div className="rating-bar-top" style={{ width: `${size}%` }} />
-      <div className="rating-bar-bottom" />
     </div>
   </span>
 );
