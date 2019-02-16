48ba17491d3119edde6da057e869c1ecc64af14f
rdkerr <rogerdkerr@gmail.com>
2019-02-15 19:57:36 -0700

    Added ActiveFilters component and test

---
+++ client/components/nav/ActiveFilters.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ActiveFilters = (props) => {
+  const classes = { ...props };
+  return (
+    <div id="active-filters">
+     
+    </div>
+  );
+};
+
+export default ActiveFilters;
---
+++ test/client/components/nav/ActiveFilters.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import ActiveFilters from '../../../../client/components/nav/ActiveFilters';
+
+describe('<ActiveFilters />', () => {
+  test('should exist', () => {
+    expect(ActiveFilters).toBeDefined();
+  });
+});
