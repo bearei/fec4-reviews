e98dca25aab07f60d0b926b4f9c8935ddc75ec62
rdkerr <rogerdkerr@gmail.com>
2019-02-16 10:43:10 -0700

    Import PropTypes

--- client/components/modal/ModalHeader.jsx
+++ client/components/modal/ModalHeader.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const ModalHeader = (props) => {
-  const classes = { ...props };
+const ModalHeader = ({ handleClose }) => {
   return (
     <div className="modal-right-el">
-      <FontAwesomeIcon onClick={classes.handleClose} className="cancel" icon="times-circle" size="lg" />
+      <FontAwesomeIcon onClick={handleClose} className="cancel" icon="times-circle" size="lg" />
       <h1 className="review-title">My Review for COMPANY PRODUCT-NAME</h1>
       <p className="required">Required fields are marked with *</p>
     </div>
   );
+};
+
+ModalHeader.propTypes = {
+  handleClose: PropTypes.func,
+};
+
+ModalHeader.defaultProps = {
+  handleClose: () => {},
 };
 
 export default ModalHeader;
