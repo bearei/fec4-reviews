27a81f092caa3682af6a4f2e01c79210f91292f3
rdkerr <rogerdkerr@gmail.com>
2019-02-17 17:47:04 -0700

    Formatting

--- client/components/modal/ModalSubmit.jsx
+++ client/components/modal/ModalSubmit.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 
-const ModalSubmit = (props) => {
-  return (
-    <div>
-      <input className="same-line" type="checkbox" />
-      <span>
-        I agree to the Bazaarvoice
-        <span> Terms of Use & Privacy Policy</span>
-        *
-      </span>
-      <div className="email-warning">
-        You may recieve emails regarding this submission.  Any
-        emails will include the ability to opt out of future communications.
-      </div>
-      <div role="button" tabIndex={0} className="button-write post-review" onKeyPress={() => {}}>Post review</div>
+const ModalSubmit = () => (
+  <div>
+    <input className="same-line" type="checkbox" />
+    <span>
+      I agree to the Bazaarvoice
+      <span> Terms of Use & Privacy Policy</span>
+      *
+    </span>
+    <div className="email-warning">
+      You may recieve emails regarding this submission.  Any
+      emails will include the ability to opt out of future communications.
     </div>
-  );
-};
+    <div role="button" tabIndex={0} className="button-write post-review" onKeyPress={() => {}}>Post review</div>
+  </div>
+);
 
 export default ModalSubmit;
