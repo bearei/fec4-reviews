907b257974155031ca0b4b7d6f71eb7b94641f00
rdkerr <rogerdkerr@gmail.com>
2019-02-21 14:50:21 -0700

    Update header

--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+// process.env.NODE_ENV
+// “NODE_ENV=test jest”
 
 const mongoUri = 'mongodb://localhost/hrei-reviews';
 mongoose.connect(mongoUri, { useNewUrlParser: true });
