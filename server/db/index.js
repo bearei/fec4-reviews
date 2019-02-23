e6cb8b6c9e02494fb3f27a0e103d013f614c5868
rdkerr <rogerdkerr@gmail.com>
2019-02-23 14:51:07 -0700

    Change db address

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoUri = process.env.NODE_ENV==='test' ? 'mongodb://127.0.0.1/test_reviews' : 'mongodb://localhost/hrei-reviews';
+const mongoUri = process.env.NODE_ENV==='test' ? 'mongodb://127.0.0.1/test_reviews' : 'mongodb+srv://raj:HREI-reviews@cluster0-wjonp.mongodb.net/test?retryWrites=true';
 mongoose.connect(mongoUri, { useNewUrlParser: true });
 const db = mongoose.connection;
 
