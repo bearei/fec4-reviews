6d57dae70f393a0a41754a6708f9cbde2f7ed18e
rdkerr <rogerdkerr@gmail.com>
2019-02-23 15:06:27 -0700

    Console log teh mongo uri

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const mongoUri = process.env.NODE_ENV==='test' ? 'mongodb://127.0.0.1/test_reviews' : 'mongodb+srv://raj:HREI-reviews@cluster0-wjonp.mongodb.net/test?retryWrites=true';
+console.log(mongoUri)
 mongoose.connect(mongoUri, { useNewUrlParser: true });
 const db = mongoose.connection;
 
