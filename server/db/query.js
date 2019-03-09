644d45d3f0c90b6a7ea8de609adc84d14fb0299c
Thomas Hsu <thomhsu@gmail.com>
2019-03-08 16:51:21 -0800

    Added createReview function

--- server/db/query.js
+++ server/db/query.js
@@ -1,24 +1,28 @@
 const db = require('./index');
 
 module.exports.findReview = function(req, res) {
-  db.then(conn => {
-    conn.query(`SELECT * FROM reviews WHERE itemId = ${req.params.itemId}`)
-      .then(data => res.status(200).send(data));
-  }) 
+  db
+    .then(conn => {
+      conn.query(`SELECT * FROM reviews WHERE itemId = ${req.params.itemId}`)
+        .then(data => res.status(200).send(data))
+        .then(conn => conn.end());
+    }) 
 };
 
 module.exports.findItem = function(req, res) {
   db.then(conn => {
     conn.query(`SELECT * FROM products WHERE itemId = ${req.params.itemId}`)
-      .then(data => res.status(200).send(data));
+      .then(data => res.status(200).send(data))
+      .then(conn => conn.end());
   }) 
 };
 
 module.exports.markHelpful = function(req, res) {
   db.then(conn => {
     conn.query(`UPDATE reviews SET helpful = helpful + 1 WHERE reviewId = ${req.params.reviewId}`)
-      .then(data => res.status(200).send(data))
-      .catch(err => console.log(err));
+      .then(ddata => res.status(200).send(data))
+      .catch(err => console.log(err))
+      .then(conn => conn.end());
   }) 
 }
 ;
@@ -26,7 +30,8 @@ module.exports.markUnhelpful = function(req, res) {
   db.then(conn => {
     conn.query(`UPDATE reviews SET helpful = helpful - 1 WHERE reviewId = ${req.params.reviewId}`)
       .then(data => res.status(200).send(data))
-      .catch(err => console.log(err));
+      .catch(err => console.log(err))
+      .then(conn => conn.end());
   }) 
 };
 
@@ -34,14 +39,16 @@ module.exports.flag = function(req, res) {
   db.then(conn => {
     conn.query(`UPDATE reviews SET flag = true WHERE reviewId = ${req.params.reviewId}`)
       .then(data => res.status(200).send(data))
-      .catch(err => console.log(err));
+      .catch(err => console.log(err))
+      .then(conn => conn.end());
   }) 
 };
 
 module.exports.createReview = function(req, res) {
   db.then(conn => {
-    conn.query(`SELECT * FROM products WHERE itemId = ${req.params.itemId}`)
-      .then(data => res.status(200).send(data));
+    conn.query(`INSERT INTO reviews (rating, title, text, recommend, name, fit, itemId) VALUES (${req.body.rating}, ${req.body.rating}, ${req.body.title}, ${req.body.text}, ${req.body.recommend}, ${req.body.name}, ${req.body.fit}, ${req.body.itemId})`)
+      .then(data => res.status(200).send(data))
+      .then(conn => conn.end());
 
       // (req, res) => {
       //   query.createReview({
