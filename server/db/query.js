606c4467dd7d140afe2a28e50a8d69c1493a62ea
Thomas Hsu <thomhsu@gmail.com>
2019-03-09 13:16:59 -0800

    Remove connection end

--- server/db/query.js
+++ server/db/query.js
@@ -5,24 +5,23 @@ module.exports.findReview = function(req, res) {
     .then(conn => {
       conn.query(`SELECT * FROM reviews WHERE itemId = ${req.params.itemId}`)
         .then(data => res.status(200).send(data))
-        .then(conn => conn.end());
-    }) 
+        .catch(err => console.log(err))
+    })
 };
 
 module.exports.findItem = function(req, res) {
   db.then(conn => {
     conn.query(`SELECT * FROM products WHERE itemId = ${req.params.itemId}`)
       .then(data => res.status(200).send(data))
-      .then(conn => conn.end());
+      .catch(err => console.log(err))
   }) 
 };
 
 module.exports.markHelpful = function(req, res) {
   db.then(conn => {
     conn.query(`UPDATE reviews SET helpful = helpful + 1 WHERE reviewId = ${req.params.reviewId}`)
-      .then(ddata => res.status(200).send(data))
-      .then(conn => conn.end())
-      .catch(err => console.log(err));
+      .then(data => res.status(200).send(data))
+      .catch(err => console.log(err))
   }) 
 }
 ;
@@ -30,8 +29,7 @@ module.exports.markUnhelpful = function(req, res) {
   db.then(conn => {
     conn.query(`UPDATE reviews SET helpful = helpful - 1 WHERE reviewId = ${req.params.reviewId}`)
       .then(data => res.status(200).send(data))
-      .then(conn => conn.end())
-      .catch(err => console.log(err));
+      .catch(err => console.log(err))
   }) 
 };
 
@@ -39,8 +37,7 @@ module.exports.flag = function(req, res) {
   db.then(conn => {
     conn.query(`UPDATE reviews SET flag = true WHERE reviewId = ${req.params.reviewId}`)
       .then(data => res.status(200).send(data))
-      .then(conn => conn.end())
-      .catch(err => console.log(err));
+      .catch(err => console.log(err))
   }) 
 };
 
@@ -48,17 +45,15 @@ module.exports.createReview = function(req, res) {
   db.then(conn => {
     conn.query(`INSERT INTO reviews (rating, title, text, recommend, name, fit, itemId, createdAt) VALUES (${req.body.rating}, '${req.body.title}', '${req.body.text}', ${req.body.recommend}, '${req.body.name}', ${req.body.fit}, ${req.body.itemId}, '${Date().toString()}')`)
       .then(data => res.status(200).send(data))
-      .then(conn => conn.end())
-      .catch(err => console.log(err));
-  }) 
+      .catch(err => console.log(err))
+  })
 };
 
 // Unused in client
 module.exports.delete = function(req, res) {
   db.then(conn => {
     conn.query(`DELETE FROM reviews where reviewId = ${req.body.reviewId};`)
-    .then(conn => conn.end())
-    .catch(err => console.log(err));
+    .catch(err => console.log(err))
   }) 
 };
 
