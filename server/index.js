07f150b83e7df914795964cf7411140c24ca9d97
Thomas Hsu <thomhsu@gmail.com>
2019-03-14 16:49:31 -0700

    Remove slash from endpoint

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3003;
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use('/:itemId/', express.static(`${__dirname}/../public`));
-app.use('/', express.static(`${__dirname}/../loaderio`));
+// app.use('/', express.static(`${__dirname}/../loaderio`));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -22,49 +22,49 @@ app.use((req, res, next) => {
 app.use(
   '/reviews/:itemId',
   proxy({
-    target:'http://52.15.132.177:3004/',
+    target:'http://52.15.132.177:3004',
     changeOrigin: true
   })
 );
 app.use(
   '/items/:itemId',
   proxy({
-    target:'http://52.15.132.177:3004/',
+    target:'http://52.15.132.177:3004',
     changeOrigin: true
   })
 );
 app.use(
   '/reviews/helpful/:reviewId',
   proxy({
-    target:'http://52.15.132.177:3004/',
+    target:'http://52.15.132.177:3004',
     changeOrigin: true
   })
 );
 app.use(
   '/reviews/notHelpful/:reviewId',
   proxy({
-    target:'http://52.15.132.177:3004/',
+    target:'http://52.15.132.177:3004',
     changeOrigin: true
   })
 );
 app.use(
   '/reviews/flag/:reviewId',
   proxy({
-    target:'http://52.15.132.177:3004/',
+    target:'http://52.15.132.177:3004',
     changeOrigin: true
   })
 );
 app.use(
   '/reviews/',
   proxy({
-    target:'http://52.15.132.177:3004/',
+    target:'http://52.15.132.177:3004',
     changeOrigin: true
   })
 );
 app.use(
   '/reviews/:reviewId',
   proxy({
-    target:'http://52.15.132.177:3004/',
+    target:'http://52.15.132.177:3004',
     changeOrigin: true
   })
 );
